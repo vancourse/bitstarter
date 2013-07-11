abf10a9d96010e1abb3a88251daea9b8bba4533a
Ravi Van <vancourse@gmail.com>
2013-07-11 17:33:39 +0000

    Updated web.js to read from index.html

--- web.js
+++ web.js
@@ -1,10 +1,13 @@
-var express = require('express');
 
+var express = require('express');
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+ var buf = readFileSync("index.html");
+  response.send(buf.toString());
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
